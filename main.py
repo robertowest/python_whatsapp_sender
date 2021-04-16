cdd199e594dc52bc83c2e47894b9a95255e0e7b2
Roberto West <roberto.west@gmail.com>
2021-04-16 09:00:02 -0300

    actualizaciones

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 from whatsender import WhatSender
+import os, time
 
-ws = WhatSender()
+CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
+ROOT_PATH = os.path.dirname( CURRENT_PATH )
 
-list = [
-    {'full_name': 'Roberto West', 'phone': '3816168251', 'name': 'Roberto', 'surname': 'West'},
-]
-ws.PassContactList(list)
-# ws.ContactList('contacts.csv')
+def elapsed_time(start_time):
+    end_time = time.time()
+    hours, rem = divmod(end_time-start_time, 3600)
+    minutes, seconds = divmod(rem, 60)
+    return "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds)
 
-try:
-    ws.SendMessage("Hola {nombre}\nSomos *Lubre SRL* tu empresa de YPF Agro.\n\nNo dudes en llamarnos.")
-    # ws.SendImage("/home/roberto/Imágenes/django-icon-0.jpg")
-finally:
-    ws.Close()
+def whatsapp_sender():
+    ws = WhatSender()
+
+    # list = [
+    #     {'full_name': 'Roberto West', 'phone': '3816168251', 'name': 'Roberto', 'surname': 'West'},
+    # ]
+    # ws.PassContactList(list)
+    ws.ContactList(os.path.join(CURRENT_PATH, "listado.csv"))
+
+    try:
+        # ws.SendMessage("Hola {nombre}\n
+        #                 Somos *Lubre SRL* tu empresa de YPF Agro.\n
+        #                 \n
+        #                 No dudes en llamarnos.")
+        ws.SendImage(os.path.join(CURRENT_PATH, "imagen.png"))
+    finally:
+        ws.Close()
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    whatsapp_sender()
+    print("Tiempo transcurrido: {}".format( elapsed_time(start_time)) )
+
